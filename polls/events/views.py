7dbfd963e40ea2b932cb4328d54a57045f4fbc35
malek <boulahdourabdelmalek@gmail.com>
2025-06-07 14:01:16 +0100

    initial page

--- polls/events/views.py
+++ polls/events/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import request
 # Create your views here.
+
+
+def index(request):
+    events = []
+ 
+    if request.method == 'POST':
+        event = request.POST.get('events')
+        events.append(event)
+    return render(request, 'index.html', {'events': events})
+    
